3be056f9d46986ce0111e6639774053ac06fcb4f
Akuni <sn102953@etu.unice.fr>
2014-11-30 23:22:49 +0100

    lib

--- src/zuul/rooms/Library.java
+++ src/zuul/rooms/Library.java
@@ -1,11 +1,14 @@
 package zuul.rooms;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * @author Nicolas Sarroche, Dorian Blanc
  */
 public class Library extends Room{
+
+
 
 	public Library(String description) {
 		super(description);
@@ -15,5 +18,11 @@ public class Library extends Room{
 
     public String readBook(){
     	return "WOW I read a book !";
+    }
+
+    public boolean checkForschedule() {
+        Random random = new Random();
+        int hour = random.nextInt(24);
+        return hour==1;
     }
 }
